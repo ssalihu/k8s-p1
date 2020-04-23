1108a0359fab46d993bfe94e71674ff833da18d5
Safir Salihu <msafirs@gmail.com>
2020-04-23 18:13:16 -0500

    Fix deploy to hub script

---
+++ deploy-to-hub.sh
@@ -0,0 +1,10 @@
+docker build -t 1after/api-java:latest -t 1after/api-java:$SHA ./api
+docker build -t 1after/k8-p1-nginx:latest -t 1after/k8-p1-nginx:$SHA  ./nginx
+docker build -t 1after/counter:latest -t 1after/counter:latest ./counter
+
+docker push 1after/api-java:latest
+docker push 1after/k8-p1-nginx:latest
+docker push 1after/counter:latest
+docker push 1after/api-java:$SHA
+docker push 1after/k8-p1-nginx:$SHA
+docker push 1after/counter:$SHA
