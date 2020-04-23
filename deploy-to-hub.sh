81dff37b44bcf36d2b2d5c9bd38946d1960199fe
Safir Salihu <msafirs@gmail.com>
2020-04-23 18:21:05 -0500

    Dummy i said not to copy paste

--- deploy-to-hub.sh
+++ deploy-to-hub.sh
@@ -1,6 +1,6 @@
 docker build -t 1after/api-java:latest -t 1after/api-java:$SHA ./api
 docker build -t 1after/k8-p1-nginx:latest -t 1after/k8-p1-nginx:$SHA  ./nginx
-docker build -t 1after/counter:latest -t 1after/counter:latest ./counter
+docker build -t 1after/counter:latest -t 1after/counter:$SHA ./counter
 
 docker push 1after/api-java:latest
 docker push 1after/k8-p1-nginx:latest
