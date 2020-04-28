e8d3a5cdc35256937e2fc6e9662e5ce9b74f6076
Safir Salihu <msafirs@gmail.com>
2020-04-28 14:22:53 -0500

    more travis refactor

--- deploy-to-aws.sh
+++ deploy-to-aws.sh
@@ -7,7 +7,7 @@ export PATH=$PATH:$HOME/.local/bin
 openssl aes-256-cbc -K $encrypted_dfba7aade086_key -iv $encrypted_dfba7aade086_iv -in config.enc -out ~/.aws/config -d
 openssl aes-256-cbc -K $encrypted_d506bd5213c4_key -iv $encrypted_d506bd5213c4_iv -in credentials.enc -out ~/.aws/credentials -d
 export AWS_PROFILE=aws-eks
-aws eks --region us-east-1 update-kubeconfig --name EmployeeTestCl
+aws eks --region us-east-1 update-kubeconfig --name EmployeeTestCluster
 # - aws configure set default.aws_access_key_id $DEFAULT_AWS_ACCESS_KEY_ID
 # - aws configure set default.aws_secret_access_key $DEFAULT_AWS_SECRET_ACCESS_KEY
 # Setup alternative AWS credentials using an AWS Named Profile for testing.
